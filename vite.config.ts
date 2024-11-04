db59e960b5b0c48a62f70094ee9889d67c3d0f07
HamzaChaudhary786 <hmzsattar99@gmail.com>
2024-11-04 01:29:12 -0800

    pagination update in search hostel page

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: '/', // Adjust this to your actual deployment path, or use '/' if at root
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
